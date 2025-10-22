51ec0853672c51c110ad2ec6a73200615d6f8446
Vlad Tomoiaga <vlad.tomoiaga@msg.group>
2025-10-22 12:44:06 +0300

    View table add

--- routes.py
+++ routes.py
@@ -9,6 +9,7 @@ routes = Blueprint("routes", __name__)
 load_dotenv()
 ACCESS_KEY = os.getenv("ACCESS_KEY")
 URL = os.getenv("URL")
+URL_EOD = os.getenv("URL_EOD")
 
 @routes.context_processor
 def inject_current_year():
@@ -27,3 +28,17 @@ def home():
     data_api = response.json()["data"]
 
     return render_template("index.html", data=data_api)
+
+@routes.route("/view/<symbol>")
+def view(symbol):
+    """This method create the route the View Stocks page of a company"""
+
+    params = {
+        "access_key": ACCESS_KEY,
+        "symbols": symbol,
+    }
+
+    response = requests.get(url=URL_EOD, params=params)
+    eod_data_api = response.json()["data"]
+
+    return render_template("view.html", eod_data=eod_data_api)
